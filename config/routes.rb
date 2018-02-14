ff10606b1720da1fcb01e2074f3202ec2ff96d74
Aslynx <anthony.pierr23@gmail.com>
2018-02-14 12:02:20 +0100

    Sorcery authentication complete

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,10 @@ Blogger::Application.routes.draw do
 
   resources :tags
 
+  resources :authors
+
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+  get 'login' => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
+
 end
